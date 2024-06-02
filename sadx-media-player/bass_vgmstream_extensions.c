d4629a0a30fd6e43f32d7e5c0152f9c339326634
PkR <PiKeyAr@users.noreply.github.com>
2024-06-02 20:38:41 +0900

    Media player, FFMPEG 7.1, merge FFMPEG and bass_vgmstream stuff

---
+++ sadx-media-player/bass_vgmstream_extensions.c
@@ -0,0 +1,346 @@
+#include "bass_vgmstream.h"
+
+#include <vgmstream.h>
+#include <stdlib.h>
+
+// Stuff removed from vgmstream
+void put_8bit(uint8_t* buf, int8_t i) {
+	buf[0] = i;
+}
+
+void put_16bitLE(uint8_t* buf, int16_t i) {
+	buf[0] = (i & 0xFF);
+	buf[1] = i >> 8;
+}
+
+void put_32bitLE(uint8_t* buf, int32_t i) {
+	buf[0] = (uint8_t)(i & 0xFF);
+	buf[1] = (uint8_t)((i >> 8) & 0xFF);
+	buf[2] = (uint8_t)((i >> 16) & 0xFF);
+	buf[3] = (uint8_t)((i >> 24) & 0xFF);
+}
+
+void put_16bitBE(uint8_t* buf, int16_t i) {
+	buf[0] = i >> 8;
+	buf[1] = (i & 0xFF);
+}
+
+void put_32bitBE(uint8_t* buf, int32_t i) {
+	buf[0] = (uint8_t)((i >> 24) & 0xFF);
+	buf[1] = (uint8_t)((i >> 16) & 0xFF);
+	buf[2] = (uint8_t)((i >> 8) & 0xFF);
+	buf[3] = (uint8_t)(i & 0xFF);
+}
+
+
+// WAV header function from vgmstream r1040 modified to store loop size
+void make_wav_header(uint8_t* buf, int32_t sample_count, int32_t sample_rate, int channels, int loop) 
+{
+	size_t bytecount;
+
+	bytecount = sample_count * channels * sizeof(sample);
+
+	/* RIFF header */
+	memcpy(buf + 0, "RIFF", 4);
+
+	/* size of RIFF */
+	put_32bitLE(buf + 4, (int32_t)(bytecount + 0x2c - 8 + loop * 0x44));
+
+	/* WAVE header */
+	memcpy(buf + 8, "WAVE", 4);
+
+	/* WAVE fmt chunk */
+	memcpy(buf + 0xc, "fmt ", 4);
+
+	/* size of WAVE fmt chunk */
+	put_32bitLE(buf + 0x10, 0x10);
+
+	/* compression code 1=PCM */
+	put_16bitLE(buf + 0x14, 1);
+
+	/* channel count */
+	put_16bitLE(buf + 0x16, channels);
+
+	/* sample rate */
+	put_32bitLE(buf + 0x18, sample_rate);
+
+	/* bytes per second */
+	put_32bitLE(buf + 0x1c, sample_rate * channels * sizeof(sample));
+
+	/* block align */
+	put_16bitLE(buf + 0x20, (int16_t)(channels * sizeof(sample)));
+
+	/* significant bits per sample */
+	put_16bitLE(buf + 0x22, sizeof(sample) * 8);
+
+	/* PCM has no extra format bytes, so we don't even need to specify a count */
+
+	/* WAVE data chunk */
+	memcpy(buf + 0x24, "data", 4);
+
+	/* size of WAVE data chunk */
+	put_32bitLE(buf + 0x28, (int32_t)bytecount);
+}
+
+// Read from memory: https://github.com/vgmstream/vgmstream/issues/662
+STREAMFILE* open_memory_streamfile(uint8_t* buf, size_t bufsize, const char* name);
+
+typedef struct {
+	STREAMFILE sf; /* pre-alloc'd part */
+
+	uint8_t* buf;
+	size_t bufsize;
+	const char* name;
+	offv_t offset;
+} MEMORY_STREAMFILE;
+
+static size_t memory_read(MEMORY_STREAMFILE* sf, uint8_t* dst, offv_t offset, size_t length) {
+	if (!dst || length <= 0 || offset < 0 || offset >= sf->bufsize)
+		return 0;
+	if (offset + length > sf->bufsize)
+		length = sf->bufsize - offset; /* clamp */
+
+	memcpy(dst, sf->buf + offset, length);
+	sf->offset = offset;
+	return length;
+}
+
+static size_t memory_get_size(MEMORY_STREAMFILE* sf) {
+	return sf->bufsize;
+}
+
+static size_t memory_get_offset(MEMORY_STREAMFILE* sf) {
+	return sf->offset;
+}
+
+static void memory_get_name(MEMORY_STREAMFILE* sf, char* buffer, size_t length) {
+	strncpy(buffer, sf->name, length);
+	buffer[length - 1] = '\0';
+}
+
+static STREAMFILE* memory_open(MEMORY_STREAMFILE* sf, const char* const filename, size_t buffersize) {
+	/* Some formats need to open companion files though, maybe pass N bufs+name per file */
+	/* also must detect "reopens", as internal processes need to clone this streamfile at times */
+	if (strcmp(filename, sf->name) == 0)
+		return open_memory_streamfile(sf->buf, sf->bufsize, sf->name); /* reopen */
+	return NULL;
+}
+
+static void memory_close(MEMORY_STREAMFILE* sf) {
+	free(sf);
+}
+
+STREAMFILE* open_memory_streamfile(uint8_t* buf, size_t bufsize, const char* name) {
+	MEMORY_STREAMFILE* this_sf = NULL;
+
+	this_sf = calloc(1, sizeof(MEMORY_STREAMFILE));
+	if (!this_sf) goto fail;
+
+	this_sf->sf.read = (void*)memory_read;
+	this_sf->sf.get_size = (void*)memory_get_size;
+	this_sf->sf.get_offset = (void*)memory_get_offset;
+	this_sf->sf.get_name = (void*)memory_get_name;
+	this_sf->sf.open = (void*)memory_open;
+	this_sf->sf.close = (void*)memory_close;
+
+	/* assumes bufs live externally during decode, otherwise malloc/memcpy and free on close */
+	this_sf->buf = buf;
+	this_sf->bufsize = bufsize;
+	this_sf->name = name;
+
+	return &this_sf->sf;
+fail:
+	free(this_sf);
+	return NULL;
+}
+
+// Conversion
+
+/* make a loop chunk for PCM .wav */
+/* buffer must be 0x44 bytes */
+void make_smpl_header(uint8_t* buf, int32_t loop_start, int32_t loop_end)
+{
+	// SMLP header
+	memcpy(buf + 0, "smpl", 4);
+	// Size of SMPL chunk (always 0x3C for one loop)
+	put_32bitLE(buf + 4, 0x3C);
+	// Manufacturer
+	put_32bitLE(buf + 8, 0);
+	// Product
+	put_32bitLE(buf + 12, 0);
+	// Sample Period
+	put_32bitLE(buf + 16, 45351); // SADX value
+	// MIDI Unity Note
+	put_32bitLE(buf + 20, 60); // SADX value
+	// MIDI pitch fraction
+	put_32bitLE(buf + 24, 0);
+	// SMPTE format
+	put_32bitLE(buf + 28, 30); // SADX value
+	// SMPTE offset
+	put_32bitLE(buf + 32, 0);
+	// Number of sample loops (always 1 in SADX)
+	put_32bitLE(buf + 36, 1);
+	// Sampler specific data (unused in SADX)
+	put_32bitLE(buf + 40, 0);
+	// Sample loop ID (always 0 in SADX)
+	put_32bitLE(buf + 44, 0);
+	// Sample loop type (always 0 in SADX)
+	put_32bitLE(buf + 48, 0);
+	// Loop start
+	put_32bitLE(buf + 52, loop_start);
+	// Loop end
+	put_32bitLE(buf + 56, loop_end);
+	// Sample loop fraction (always 0 in SADX)
+	put_32bitLE(buf + 60, 0);
+	// Sample loop repeat count (always infinite?)
+	put_32bitLE(buf + 64, 0);
+}
+
+int Convert(VGMSTREAM* vgmstream, unsigned char* outputdata)
+{
+	int result = 1; // 0 if there is no error
+	sample* buf = NULL;
+	int32_t len;
+	const int BUFSIZE = 4000; // PCM sample size
+	int position = 0x2C; // Buffer seeking
+
+	// Init
+	if (!vgmstream)
+	{
+		return 1;
+	}
+
+	// Allocate buffer
+	buf = malloc(BUFSIZE * sizeof(sample) * vgmstream->channels);
+	if (buf == NULL)
+		return 1;
+
+	len = get_vgmstream_play_samples(0, 0, 0, vgmstream);
+	if (vgmstream->loop_end_sample > len)
+		len += (vgmstream->loop_end_sample - len);
+
+	// Add WAV header
+	make_wav_header(outputdata, len, vgmstream->sample_rate, vgmstream->channels, vgmstream->loop_flag);
+	
+	// Decode data
+	for (int i = 0; i < len; i += BUFSIZE)
+	{
+		int toget = BUFSIZE;
+		if (i + BUFSIZE > len)
+			toget = len - i;
+
+		render_vgmstream(buf, toget, vgmstream);
+		swap_samples_le(buf, vgmstream->channels * toget);
+		memcpy(&outputdata[position], buf, sizeof(sample) * vgmstream->channels * toget);
+		position += sizeof(sample) * vgmstream->channels * toget;
+	}
+
+	// Add loop header
+	if (vgmstream->loop_flag == 1)
+	{
+		make_smpl_header(&outputdata[position], vgmstream->loop_start_sample, vgmstream->loop_end_sample);
+	}
+
+	// Cleanup
+	free(buf);
+	buf = NULL;
+	return 0;
+}
+
+// API functions
+BASS_VGMSTREAM_API void* BASS_VGMSTREAM_InitVGMStreamFromMemory(void* data, int size, const char* name)
+{
+	STREAMFILE* sf = open_memory_streamfile(data, size, name);
+	return (void*)init_vgmstream_from_STREAMFILE(sf);
+}
+
+BASS_VGMSTREAM_API void BASS_VGMSTREAM_CloseVGMStream(void* vgmstream)
+{
+	close_vgmstream((VGMSTREAM*)vgmstream);
+}
+
+BASS_VGMSTREAM_API int BASS_VGMSTREAM_GetVGMStreamOutputSize(void* vgmstream)
+{
+	VGMSTREAM* stream = (VGMSTREAM*)vgmstream;
+	int numsample = get_vgmstream_play_samples(0, 0, 0, stream);
+	if (stream->loop_end_sample > numsample)
+		numsample += (stream->loop_end_sample - numsample);
+	return numsample * sizeof(sample) * stream->channels + 0x2C + (stream->loop_flag ? 0x44 : 0); // + WAV header + possibly smpl header for 1 loop
+}
+
+BASS_VGMSTREAM_API int BASS_VGMSTREAM_ConvertVGMStreamToWav(void* vgmstream, unsigned char* outputdata)
+{
+	return Convert((VGMSTREAM*)vgmstream, outputdata);
+}
+
+int main(int argc, char* argv[])
+{
+	const char* srcFile = "test.adx";
+	const char* dstFile = "test.wav";
+	int inputSize;
+	int outputSize = 0;
+	void* InputBuffer;
+	unsigned char* OutputBuffer = NULL;
+	double loop_count = 10.0;
+	double fade_seconds = 0.0;
+	double fade_delay_seconds = 0.0;
+	int fade_samples = 0;
+	FILE* f = NULL;
+	errno_t err;
+
+	// Open input file
+	err = fopen_s(&f, srcFile, "rb");
+	if (err || f == NULL)
+	{
+		printf("Input file error\n");
+		return;
+	}
+	fseek(f, 0L, SEEK_END);
+	
+	// Get input size and allocate memory
+	inputSize = ftell(f);
+	fseek(f, 0L, SEEK_SET);
+	
+	// Read source file into memory
+	InputBuffer = malloc(inputSize);
+	if (InputBuffer == NULL)
+	{
+		printf("Could not allocate input buffer\n");
+		return;
+	}
+	fread(InputBuffer, 1, inputSize, f);
+	fclose(f);
+
+	// Create vgmstream from memory
+	VGMSTREAM* vgmstream = BASS_VGMSTREAM_InitVGMStreamFromMemory(InputBuffer, inputSize, srcFile);
+
+	// Print loop info
+	if (vgmstream->loop_flag)
+	{
+		printf("Loop start: %u\n", vgmstream->loop_start_sample);
+		printf("Loop end: %u\n", vgmstream->loop_end_sample);
+	}
+
+	// Get output size and allocate memory
+	outputSize = BASS_VGMSTREAM_GetVGMStreamOutputSize(vgmstream);
+	OutputBuffer = malloc(outputSize);
+
+	// Convert data
+	Convert(vgmstream, OutputBuffer);		
+
+	// Write output file
+	err = fopen_s(&f, dstFile, "wb");
+	if (err || f == NULL)
+	{
+		printf("Output file error\n");
+		return;
+	}
+	if (OutputBuffer == NULL)
+	{
+		printf("Output buffer empty\n");
+		return;
+	}
+	fwrite(OutputBuffer, outputSize, 1, f);
+	fclose(f);
+	return 0;
+}
---
+++ sadx-media-player/video.cpp
@@ -0,0 +1,503 @@
+// TODO: Improve prerendering (ditch vectors, second thread, preallocate buffers)
+
+#include <DShow.h>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include "bass_vgmstream.h"
+#include "video.h"
+
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libswresample/swresample.h"
+}
+
+char msg[4096];
+
+class VideoPlayer
+{
+private:
+	std::thread* pVideoThread = nullptr;
+
+	AVFormatContext* pFormatContext = nullptr;
+	AVPacket* pPacket = nullptr;
+	AVFrame* pFrame = nullptr;
+
+	AVCodecContext* pVideoCodecContext = nullptr;
+	SwsContext* pSwsContext = nullptr;
+	AVFrame* pVideoFrame = nullptr;
+	uint8_t* framebuffer = nullptr;
+
+	AVCodecContext* pAudioCodecContext = nullptr;
+	SwrContext* pSwrContext = nullptr;
+	AVFrame* pAudioFrame = nullptr;
+	HSTREAM BassHandle = NULL;
+
+	int video_stream_index = -1;
+	int audio_stream_index = -1;
+
+	unsigned int width = 0;
+	unsigned int height = 0;
+
+	bool opened = false;
+	bool play = false;
+	bool finished = false;
+	bool update = false;
+
+	std::chrono::steady_clock::time_point real_time;
+	double video_time = 0.0;
+	double next = 0.0;
+
+	struct Frame
+	{
+		double video_time;
+		uint8_t* data;
+		size_t size;
+	};
+
+	std::vector<Frame> video_frames;
+
+	void DecodeAudio(AVStream* pStream)
+	{
+		if (avcodec_send_packet(pAudioCodecContext, pPacket) < 0)
+		{
+			return;
+		}
+
+		if (avcodec_receive_frame(pAudioCodecContext, pFrame) < 0)
+		{
+			return;
+		}
+
+		if (swr_convert_frame(pSwrContext, pAudioFrame, pFrame) < 0)
+		{
+			OutputDebugStringA("[video] Failed to convert audio frame.\n");
+			return;
+		}
+
+		int size = av_samples_get_buffer_size(&pAudioFrame->linesize[0], pAudioFrame->ch_layout.nb_channels, pAudioFrame->nb_samples, AV_SAMPLE_FMT_FLT, 0);
+		if (size < 0)
+		{
+			OutputDebugStringA("[video] Failed to send audio buffer to BASS.\n");
+			return;
+		}
+
+		BASS_StreamPutData(BassHandle, pAudioFrame->data[0], size);
+	}
+
+	void DecodeVideo(AVStream* pStream)
+	{
+		if (avcodec_send_packet(pVideoCodecContext, pPacket) < 0)
+		{
+			return;
+		}
+
+		if (avcodec_receive_frame(pVideoCodecContext, pFrame) < 0)
+		{
+			return;
+		}
+
+		sws_scale(pSwsContext,
+			pFrame->data,
+			pFrame->linesize,
+			0,
+			pFrame->height,
+			pVideoFrame->data,
+			pVideoFrame->linesize);
+
+		size_t size = width * height * 4;
+		auto data = (uint8_t*)malloc(size);
+		memcpy(data, pVideoFrame->data[0], size);
+
+		Frame frame;
+		frame.video_time = pFrame->best_effort_timestamp * av_q2d(pStream->time_base);
+		frame.size = size;
+		frame.data = data;
+
+		video_frames.push_back(frame);
+	}
+
+	void Decode()
+	{
+		// Queue frames
+		if (video_frames.size() < 5)
+		{
+			int ret = av_read_frame(pFormatContext, pPacket);
+
+			if (ret < 0)
+			{
+				if (ret == AVERROR_EOF)
+				{
+					finished = true; // TODO: wait until queue has been rendered
+				}
+			}
+
+			if (pPacket->stream_index == video_stream_index)
+			{
+				DecodeVideo(pFormatContext->streams[video_stream_index]);
+			}
+			else if (pPacket->stream_index == audio_stream_index)
+			{
+				DecodeAudio(pFormatContext->streams[audio_stream_index]);
+			}
+
+			av_packet_unref(pPacket);
+		}
+
+		// Run video (TODO: different thread)
+		if (video_frames.size())
+		{
+			if (video_time >= video_frames[0].video_time)
+			{
+				update = false;
+				memcpy(framebuffer, video_frames[0].data, video_frames[0].size);
+				update = true;
+
+				free(video_frames[0].data);
+				video_frames.erase(video_frames.begin());
+			}
+		}
+	}
+
+	void m_VideoThread()
+	{
+		while (1)
+		{
+			auto now = std::chrono::steady_clock::now();
+			auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - real_time);
+
+			if (!opened)
+				break;
+
+			if (elapsed.count() == 0)
+				continue;
+
+			real_time = now;
+
+			if (!play || finished)
+				continue;
+
+			video_time += (double)elapsed.count() * 0.90;
+
+			Decode();
+		}
+	}
+
+	static void VideoThread(VideoPlayer* _this)
+	{
+		_this->m_VideoThread();
+	}
+
+public:
+	unsigned int Width()
+	{
+		return width;
+	}
+
+	unsigned int Height()
+	{
+		return height;
+	}
+
+	bool Finished()
+	{
+		return finished;
+	}
+
+	bool GetFrameBuffer(uint8_t* pBuffer)
+	{
+		if (opened && update)
+		{
+			memcpy(pBuffer, framebuffer, width * height * 4);
+			update = false;
+			return true;
+		}
+		return false;
+	}
+
+	void Play()
+	{
+		play = true;
+		if (BassHandle)
+		{
+			BASS_ChannelPlay(BassHandle, FALSE);
+		}
+	}
+
+	void Pause()
+	{
+		play = false;
+		if (BassHandle)
+		{
+			BASS_ChannelStop(BassHandle);
+		}
+	}
+
+	bool Open(const char* path, bool sfd)
+	{
+		if (opened)
+		{
+			Close();
+		}
+
+		pFormatContext = avformat_alloc_context();
+		if (!pFormatContext)
+		{
+			OutputDebugStringA("[video] Failed to initialize ffmpeg.\n");
+			return false;
+		}
+
+		if (sfd)
+		{
+			OutputDebugStringA("[video] SFD compatibility mode.\n");
+			pFormatContext->audio_codec_id = AV_CODEC_ID_ADPCM_ADX;
+		}
+
+		if (avformat_open_input(&pFormatContext, path, NULL, NULL) != 0 ||
+			avformat_find_stream_info(pFormatContext, NULL) < 0)
+		{
+			sprintf(msg, "[video] Failed to open % s.\n", path);
+			OutputDebugStringA(msg);
+			return false;
+		}
+
+		avio_seek(pFormatContext->pb, 0, SEEK_SET);
+
+		video_stream_index = av_find_best_stream(pFormatContext, AVMEDIA_TYPE_VIDEO, -1, -1, nullptr, 0);
+		if (video_stream_index < 0)
+		{
+			OutputDebugStringA("[video] No video stream found.\n");
+			return false;
+		}
+
+		const AVCodec* pVideoCodec = avcodec_find_decoder(pFormatContext->streams[video_stream_index]->codecpar->codec_id);
+		pFormatContext->video_codec = pVideoCodec;
+
+		pVideoCodecContext = avcodec_alloc_context3(pVideoCodec);
+		if (!pVideoCodecContext)
+		{
+			OutputDebugStringA("[video] Failed to initialize video codec context.\n");
+			return false;
+		}
+
+		AVStream* pVideoStream = pFormatContext->streams[video_stream_index];
+
+		avformat_seek_file(pFormatContext, 0, 0, 0, pFormatContext->streams[0]->duration, 0);
+
+		if (avcodec_parameters_to_context(pVideoCodecContext, pVideoStream->codecpar) < 0 ||
+			avcodec_open2(pVideoCodecContext, pVideoCodec, NULL) < 0)
+		{
+			OutputDebugStringA("[video] Failed to initialize video codec.\n");
+			return false;
+		}
+
+		width = pVideoCodecContext->width;
+		height = pVideoCodecContext->height;
+
+		pSwsContext = sws_getContext(
+			pVideoCodecContext->width,
+			pVideoCodecContext->height,
+			pVideoCodecContext->pix_fmt,
+			width,
+			height,
+			AV_PIX_FMT_BGRA,
+			SWS_BICUBIC,
+			NULL,
+			NULL,
+			NULL);
+
+		if (pSwsContext == NULL)
+		{
+			OutputDebugStringA("[video] Failed to initialize video conversion.\n");
+			return false;
+		}
+
+		pPacket = av_packet_alloc();
+		if (!pPacket)
+		{
+			OutputDebugStringA("[video] Failed to allocate packet\n");
+			return false;
+		}
+
+		pFrame = av_frame_alloc();
+		if (!pFrame)
+		{
+			OutputDebugStringA("[video] Failed to allocate packet frame.\n");
+			return false;
+		}
+
+		pVideoFrame = av_frame_alloc();
+		if (!pVideoFrame)
+		{
+			OutputDebugStringA("[video] Failed to allocate video output frame.\n");
+			return false;
+		}
+
+		pVideoFrame->format = AV_PIX_FMT_BGRA;
+		pVideoFrame->width = width;
+		pVideoFrame->height = height;
+
+		if (av_frame_get_buffer(pVideoFrame, 0) < 0)
+		{
+			OutputDebugStringA("[video] Failed to allocate video output frame buffer.\n");
+			return false;
+		}
+
+		framebuffer = (uint8_t*)av_malloc(width * height * 4);
+		if (!framebuffer)
+		{
+			OutputDebugStringA("[video] Failed to allocate frame buffer.\n");
+			return false;
+		}
+
+		audio_stream_index = av_find_best_stream(pFormatContext, AVMEDIA_TYPE_AUDIO, -1, -1, nullptr, 0);
+		if (audio_stream_index >= 0)
+		{
+			const AVCodec* pAudioCodec = avcodec_find_decoder(pFormatContext->streams[audio_stream_index]->codecpar->codec_id);
+			pFormatContext->audio_codec = pAudioCodec;
+
+			pAudioCodecContext = avcodec_alloc_context3(pAudioCodec);
+			if (!pAudioCodecContext)
+			{
+				OutputDebugStringA("[video] Failed to initialize audio codec context.\n");
+				return false;
+			}
+
+			AVStream* pAudioStream = pFormatContext->streams[audio_stream_index];
+
+			if (avcodec_parameters_to_context(pAudioCodecContext, pAudioStream->codecpar) < 0 ||
+				avcodec_open2(pAudioCodecContext, pAudioCodec, NULL) < 0)
+			{
+				OutputDebugStringA("[video] failed to initialize audio codec.\n");
+				return false;
+			}
+
+			// Initialize resampler
+			if (swr_alloc_set_opts2(&pSwrContext, &pAudioCodecContext->ch_layout, AV_SAMPLE_FMT_FLT, pAudioCodecContext->sample_rate,
+				&pAudioCodecContext->ch_layout, (AVSampleFormat)pAudioStream->codecpar->format, pAudioStream->codecpar->sample_rate, 0, nullptr) < 0)
+			{
+				OutputDebugStringA("[video] Failed to initialize audio conversion.\n");
+				return false;
+			}
+
+			// Force set audio channel layout for SFD
+			if (sfd)
+			{
+				pAudioCodecContext->ch_layout.order = AV_CHANNEL_ORDER_NATIVE;
+				pAudioCodecContext->ch_layout.u.mask = AV_CH_FRONT_LEFT | AV_CH_FRONT_RIGHT;
+			}
+
+			pAudioFrame = av_frame_alloc();
+			if (!pAudioFrame)
+			{
+				OutputDebugStringA("[video] Failed to allocate audio output frame.\n");
+				return false;
+			}
+
+			pAudioFrame->sample_rate = pAudioCodecContext->sample_rate;
+			pAudioFrame->ch_layout = pAudioCodecContext->ch_layout;
+			pAudioFrame->format = AV_SAMPLE_FMT_FLT;
+
+			BassHandle = BASS_StreamCreate(pAudioCodecContext->sample_rate, pAudioCodecContext->ch_layout.nb_channels, BASS_SAMPLE_FLOAT, STREAMPROC_PUSH, NULL);
+			if (!BassHandle)
+			{
+				OutputDebugStringA("[video] Failed to initialize audio library.");
+				return false;
+			}
+
+			BASS_ChannelPlay(BassHandle, FALSE);
+		}
+
+		video_time = pVideoStream->start_time * av_q2d(pVideoStream->time_base);
+		opened = true;
+
+		real_time = std::chrono::steady_clock::now();
+		pVideoThread = new std::thread(VideoThread, this);
+		return true;
+	}
+
+	void Close()
+	{
+		if (opened)
+		{
+			play = false;
+			opened = false;
+			finished = false;
+
+			if (pVideoThread)
+			{
+				pVideoThread->join();
+				delete pVideoThread;
+				pVideoThread = nullptr;
+			}
+
+			if (pFormatContext) avformat_close_input(&pFormatContext);
+			if (pPacket) av_packet_free(&pPacket);
+			if (pFrame) av_frame_free(&pFrame);
+
+			if (pVideoCodecContext) avcodec_free_context(&pVideoCodecContext);
+			if (pSwsContext) sws_freeContext(pSwsContext);
+			if (pVideoFrame) av_frame_free(&pVideoFrame);
+			if (framebuffer) av_free(framebuffer);
+
+			if (pAudioCodecContext) avcodec_free_context(&pAudioCodecContext);
+			if (pSwrContext) swr_free(&pSwrContext);
+			if (pAudioFrame) av_frame_free(&pAudioFrame);
+			if (BassHandle) { BASS_StreamFree(BassHandle); BassHandle = NULL; };
+		}
+	}
+
+	VideoPlayer() = default;
+	~VideoPlayer()
+	{
+		Close();
+	}
+};
+
+static VideoPlayer player;
+
+extern "C"
+{
+	__declspec(dllexport) void ffPlayerPlay()
+	{
+		return player.Play();
+	}
+
+	__declspec(dllexport) void ffPlayerPause()
+	{
+		return player.Pause();
+	}
+
+	__declspec(dllexport) bool ffPlayerFinished()
+	{
+		return player.Finished();
+	}
+
+	__declspec(dllexport) bool ffPlayerOpen(const char* path, bool sfd)
+	{
+		return player.Open(path, sfd);
+	}
+
+	__declspec(dllexport) void ffPlayerClose()
+	{
+		return player.Close();
+	}
+
+	__declspec(dllexport) bool ffPlayerGetFrameBuffer(unsigned char* pBuffer)
+	{
+		return player.GetFrameBuffer(pBuffer);
+	}
+
+	__declspec(dllexport) unsigned int ffPlayerWidth()
+	{
+		return player.Width();
+	}
+
+	__declspec(dllexport) unsigned int ffPlayerHeight()
+	{
+		return player.Height();
+	}
+}
